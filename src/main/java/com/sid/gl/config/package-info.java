e4e41daf646de9e7923c7b3cc996149e8fbb5501
seyeadamaUASZ <a.seye3777@zig.univ.sn>
2023-05-06 23:52:43 +0200

    Initial version of miniecom generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/sid/gl/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.sid.gl.config;
---
+++ src/main/java/com/sid/gl/web/rest/errors/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Specific errors used with Zalando's "problem-spring-web" library.
+ *
+ * More information on https://github.com/zalando/problem-spring-web
+ */
+package com.sid.gl.web.rest.errors;
